3afa263d5690fc6e89728658caa701726dbd9484
jank_ <jank_329@hotmail.com>
2020-07-29 17:37:27 -0500

    Adding api-key variable

--- backend/client/src/actions/fixturesActions.js
+++ backend/client/src/actions/fixturesActions.js
@@ -3,11 +3,13 @@ const TEAM_FIXTURE_REQUEST = 'TEAM_FIXTURE_REQUEST';
 const TEAM_FIXTURE_SUCCESS = 'TEAM_FIXTURE_SUCCESS';
 const TEAM_FIXTURE_FAIL = 'TEAM_FIXTURE_FAIL';
 
+import {api_key} from '../config'
+
 
 const fixtureDetailAction = (index) => async(dispatch) =>{
     try{
         dispatch({type: TEAM_FIXTURE_REQUEST });
-        const {data} = await axios.get(`https://soccer.sportmonks.com/api/v2.0/fixtures/${index}?api_token=SfQeCCmTu5DYuPGUCfkt35zhHafVze2nmNeen07xp5GyldU2YQ3WqkimCjI0&include=league,localTeam,visitorTeam`);
+        const {data} = await axios.get(`https://soccer.sportmonks.com/api/v2.0/fixtures/${index}?api_token=${api_key}&include=league,localTeam,visitorTeam`);
         let listOfAttributes = data.data
         // let coachInfo = listOfAttributes.coach;
 
--- backend/client/src/config.js
+++ backend/client/src/config.js
@@ -1,3 +1,4 @@
 export const imageUrl = process.env.REACT_APP_IMAGE_URL || process.env.NODE_ENV === 'production'? 'https://eurofutbol-deployed.herokuapp.com/'
 : 'http://localhost:8000';
 export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
+export const api_key = process.env.API_TOKEN;
